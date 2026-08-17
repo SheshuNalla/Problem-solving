8da7c393cde0ea24b24829ab16b3ce9227026ae9
SheshuNalla <sheshunalla002@gmail.com>
2026-08-17 22:38:47 +0530

    TwoSum-2

---
+++ TwoPointers/TwoSum-2.py
@@ -0,0 +1,16 @@
+def TwoSum(numbers, target):
+    left = 0
+    right = len(numbers) - 1
+    while left < right:
+        total = numbers[left] + numbers[right]
+        if total == target:
+            return [left+1, right+1]
+        if total < target:
+            left += 1
+        else:
+            right -= 1
+
+numbers = list(map(int, input().split()))
+target = int(input())
+
+print(TwoSum(numbers, target))
