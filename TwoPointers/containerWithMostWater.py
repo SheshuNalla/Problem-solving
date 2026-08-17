5413a5242d64363cfa2c4fdd3343df05cc363190
SheshuNalla <sheshunalla002@gmail.com>
2026-08-17 19:39:11 +0530

    Container water

---
+++ TwoPointers/containerWithMostWater.py
@@ -0,0 +1,19 @@
+def maxArea(heights):
+    max_area = 0
+    left = 0
+    right = len(heights) - 1
+    while left < right:
+        width = right - left
+        water_height = min(heights[left], heights[right])
+        area = width * water_height
+        max_area = max(area, max_area)
+        if heights[left] < heights[right]:
+            left += 1
+        else:
+            right -= 1
+    return max_area
+
+
+heights = list(map(int, input().split()))
+
+print(maxArea(heights))
