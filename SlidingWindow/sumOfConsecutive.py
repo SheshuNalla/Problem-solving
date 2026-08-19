36f183e77af7bc37d3ad2b400617e92887e09699
SheshuNalla <sheshunalla002@gmail.com>
2026-08-19 13:25:35 +0530

    sliding window

---
+++ SlidingWindow/sumOfConsecutive.py
@@ -0,0 +1,9 @@
+def sumOfConsecutive(nums, k):
+    window_sum = sum(nums[:k])
+    max_sum = window_sum
+    for right in range(k, len(nums)):
+        window_sum += nums[right]
+        window_sum -= nums[right - k]
+        max_sum = max(max_sum, window_sum)
+    return max_sum
+
